1912aae15a2ff7f288394a0bd553eeb8bbee0b55
Pongpanot Chantana <75673766+munichPMN@users.noreply.github.com>
2024-05-04 16:53:20 +0700

    Update hierarchi.js
    add select2

--- js/hierarchi.js
+++ js/hierarchi.js
@@ -1,101 +1,212 @@
 /**
  * Hierarchi
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Pongpanot Chantana
  * GitHub: https://github.com/munichPMN/AddressSelector/
  * Description: JavaScript Library for creating hierarchical dropdown menus.
  */
 
-        var Hierarchi = (function () {
-            class Select {
-                constructor(options) {
-                    this.data = options.data;
-                    this.selectors = options.selectors;
-                    this.placeholders = options.placeholders || [];
-                    this.defaults = options.defaults || []; // New defaults parameter
-                    this.populateSelects();
-                    this.disableSelectsFromIndex(1);
-                }
+var Hierarchi = (function () {
+  class Select {
+    constructor(options) {
+      this.data = options.data;
+      this.selectors = options.selectors;
+      this.placeholders = options.placeholders || [];
+      this.defaults = options.defaults || []; // New defaults parameter
+      this.populateSelects();
+      this.disableSelectsFromIndex(1);
+    }
 
-                populateSelects() {
-                    for (let i = 0; i < this.selectors.length; i++) {
-                        const selectElement = document.querySelector(this.selectors[i]);
-                        this.populateOptions(selectElement, this.data, this.placeholders[i]);
-                        selectElement.addEventListener("change", () => {
-                            this.updateSelects(i, selectElement.value);
-                        });
-                    }
-                }
+    populateSelects() {
+      for (let i = 0; i < this.selectors.length; i++) {
+        const selectElement = document.querySelector(this.selectors[i]);
+        this.populateOptions(selectElement, this.data, this.placeholders[i]);
+        selectElement.addEventListener("change", () => {
+          this.updateSelects(i, selectElement.value);
+        });
+      }
+    }
 
-                populateOptions(selectElement, data, placeholder) {
-                    selectElement.innerHTML = `<option value='' selected disabled>${placeholder || 'Select'}</option>`;
-                    selectElement.disabled = false;
-                    for (const key in data) {
-                        if (data.hasOwnProperty(key)) {
-                            const option = document.createElement("option");
-                            option.value = key;
-                            option.text = key;
-                            selectElement.appendChild(option);
-                        }
-                    }
-                }
+    populateOptions(selectElement, data, placeholder) {
+      selectElement.innerHTML = `<option value='' selected disabled>${placeholder || "Select"}</option>`;
+      selectElement.disabled = false;
+      for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+          const option = document.createElement("option");
+          option.value = key;
+          option.text = key;
+          selectElement.appendChild(option);
+        }
+      }
+    }
 
+    updateSelects(startIndex, selectedValue) {
+      const nextIndex = startIndex + 1;
+      if (nextIndex >= this.selectors.length) return;
+      for (let i = nextIndex; i < this.selectors.length; i++) {
+        const selectElement = document.querySelector(this.selectors[i]);
+        selectElement.innerHTML = `<option value='' selected disabled>${this.placeholders[i] || "Select"}</option>`;
+        selectElement.disabled = true;
+      }
+      const nextSelector = this.selectors[nextIndex];
+      const nextSelectElement = document.querySelector(nextSelector);
+      let data = this.data;
+      for (let i = 0; i <= startIndex; i++) {
+        data = data[document.querySelector(this.selectors[i]).value];
+      }
+      if (data) {
+        if (typeof data === "object") {
+          this.populateOptions(
+            nextSelectElement,
+            data,
+            this.placeholders[nextIndex],
+          );
+          if (nextSelectElement.value !== "") {
+            this.updateSelects(nextIndex, nextSelectElement.value); // Recursive call for further selects
+          }
+        } else {
+          nextSelectElement.disabled = false;
+          const option = document.createElement("option");
+          option.value = data;
+          option.text = data;
+          nextSelectElement.appendChild(option);
+          option.selected = true;
+        }
+      } else {
+        nextSelectElement.innerHTML = `<option value='' selected disabled>${this.placeholders[nextIndex] || "Select"}</option>`;
+      }
+    }
 
-                updateSelects(startIndex, selectedValue) {
-                    const nextIndex = startIndex + 1;
-                    if (nextIndex >= this.selectors.length) return;
-                    for (let i = nextIndex; i < this.selectors.length; i++) {
-                        const selectElement = document.querySelector(this.selectors[i]);
-                        selectElement.innerHTML = `<option value='' selected disabled>${this.placeholders[i] || 'Select'}</option>`;
-                        selectElement.disabled = true;
-                    }
-                    const nextSelector = this.selectors[nextIndex];
-                    const nextSelectElement = document.querySelector(nextSelector);
-                    let data = this.data;
-                    for (let i = 0; i <= startIndex; i++) {
-                        data = data[document.querySelector(this.selectors[i]).value];
-                    }
-                    if (data) {
-                        if (typeof data === "object") {
-                            this.populateOptions(nextSelectElement, data, this.placeholders[nextIndex]);
-                            if (nextSelectElement.value !== '') {
-                                this.updateSelects(nextIndex, nextSelectElement.value); // Recursive call for further selects
-                            }
-                        } else {
-                            nextSelectElement.disabled = false;
-                            const option = document.createElement("option");
-                            option.value = data;
-                            option.text = data;
-                            nextSelectElement.appendChild(option);
-                            option.selected = true;
-                        }
-                    } else {
-                        nextSelectElement.innerHTML = `<option value='' selected disabled>${this.placeholders[nextIndex] || 'Select'}</option>`;
-                    }
-                }
+    disableSelectsFromIndex(startIndex) {
+      const fselectElement = document.querySelector(this.selectors[0]);
+      if (typeof this.defaults[0] !== "undefined" && this.defaults[0] !== "") {
+        for (var i = 0; i < fselectElement.options.length; i++) {
+          console.log(fselectElement);
+          if (fselectElement.options[i].value === this.defaults[0]) {
+            fselectElement.options[i].selected = true;
+            break;
+          }
+        }
+      }
+      for (let i = startIndex; i < this.selectors.length; i++) {
+        const selectElement = document.querySelector(this.selectors[i]);
+        selectElement.innerHTML = `<option value='' selected disabled>${this.placeholders[i] || "Select"}</option>`;
+        if (
+          typeof this.defaults[0] !== "undefined" &&
+          this.defaults[0] !== ""
+        ) {
+          selectElement.innerHTML = `<option value='${this.defaults[i]}' selected>${this.defaults[i]}</option>`;
+        }
+        selectElement.disabled = true;
+      }
+    }
+  }
+  class Select2 {
+    constructor(options) {
+      this.data = options.data;
+      this.selectors = options.selectors;
+      this.placeholders = options.placeholders || [];
+      this.defaults = options.defaults || [];
+      this.populateSelects();
+      this.disableSelectsFromIndex(1);
+    }
 
-                disableSelectsFromIndex(startIndex) {
-                    const fselectElement = document.querySelector(this.selectors[0]);
-                    if (typeof this.defaults[0] !== 'undefined' && this.defaults[0] !== '') {
-                        for (var i = 0; i < fselectElement.options.length; i++) {
-                            console.log(fselectElement)
-                            if (fselectElement.options[i].value === this.defaults[0]) {
-                                fselectElement.options[i].selected = true;
-                                break;
-                            }
-                        }
-                    }
-                    for (let i = startIndex; i < this.selectors.length; i++) {
-                        const selectElement = document.querySelector(this.selectors[i]);
-                        selectElement.innerHTML = `<option value='' selected disabled>${this.placeholders[i] || 'Select'}</option>`;
-                        if (typeof this.defaults[0] !== 'undefined' && this.defaults[0] !== '') {
-                        selectElement.innerHTML = `<option value='${this.defaults[i]}' selected>${this.defaults[i]}</option>`;
-                        }
-                        selectElement.disabled = true;
-                    }
-                }
-            }
-            return {
-                Select: Select
-            };
-        })();
+    populateSelects() {
+      for (let i = 0; i < this.selectors.length; i++) {
+        const selectElement = $(this.selectors[i]);
+        this.populateOptions(selectElement, this.data, this.placeholders[i]);
+        selectElement.on("change", () => {
+          this.updateSelects(i, selectElement.val());
+        });
+
+        selectElement.select2();
+      }
+    }
+
+    populateOptions(selectElement, data, placeholder) {
+      selectElement.empty();
+      selectElement.append(
+        `<option value='' disabled selected>${placeholder || "Select"}</option>`,
+      );
+      for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+          selectElement.append(`<option value='${key}'>${key}</option>`);
+        }
+      }
+    }
+
+    updateSelects(startIndex, selectedValue) {
+      const nextIndex = startIndex + 1;
+      if (nextIndex >= this.selectors.length) return;
+      for (let i = nextIndex; i < this.selectors.length; i++) {
+        const selectElement = $(this.selectors[i]);
+        selectElement.empty();
+        selectElement.append(
+          `<option value='' disabled selected>${this.placeholders[i] || "Select"}</option>`,
+        );
+        selectElement.prop("disabled", true);
+        selectElement.prop("selectedIndex", 0);
+        selectElement.trigger("change.select2");
+      }
+      const nextSelector = this.selectors[nextIndex];
+      const nextSelectElement = $(nextSelector);
+      let data = this.data;
+      for (let i = 0; i <= startIndex; i++) {
+        data = data[$(this.selectors[i]).val()];
+      }
+      if (data) {
+        console.log(typeof data);
+        if (typeof data === "object") {
+          this.populateOptions(
+            nextSelectElement,
+            data,
+            this.placeholders[nextIndex],
+          );
+          if (nextSelectElement.val() !== "") {
+            this.updateSelects(nextIndex, nextSelectElement.val());
+          }
+
+          nextSelectElement.prop("disabled", false);
+        } else {
+          nextSelectElement.prop("disabled", false);
+          nextSelectElement.append(`<option value='${data}'>${data}</option>`);
+          nextSelectElement.val(data).trigger("change.select2");
+        }
+      } else {
+        nextSelectElement.empty();
+        nextSelectElement.append(
+          `<option value='' disabled selected>${this.placeholders[nextIndex] || "Select"}</option>`,
+        );
+      }
+    }
+
+    disableSelectsFromIndex(startIndex) {
+      const fselectElement = $(this.selectors[0]);
+      if (typeof this.defaults[0] !== "undefined" && this.defaults[0] !== "") {
+        fselectElement.val(this.defaults[0]).trigger("change.select2");
+      }
+      for (let i = startIndex; i < this.selectors.length; i++) {
+        const selectElement = $(this.selectors[i]);
+        selectElement.empty();
+        selectElement.append(
+          `<option value='' disabled selected>${this.placeholders[i] || "Select"}</option>`,
+        );
+        if (
+          typeof this.defaults[0] !== "undefined" &&
+          this.defaults[0] !== ""
+        ) {
+          selectElement.append(
+            `<option value='${this.defaults[i]}' selected>${this.defaults[i]}</option>`,
+          );
+        }
+        selectElement.prop("disabled", true);
+        selectElement.trigger("change.select2");
+        selectElement.select2({ disabled: true });
+      }
+    }
+  }
+  return {
+    Select: Select,
+    Select2: Select2,
+  };
+})();
